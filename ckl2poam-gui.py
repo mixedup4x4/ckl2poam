f7293d2b7c54cda91918ab2a3fb5daa4e420fa00
jabbalover <jabbalover@asafewebsite.com>
2012-08-10 16:38:59 -0400

    Added 'Finding Details' column

--- ckl2poam-gui.py
+++ ckl2poam-gui.py
@@ -65,8 +65,8 @@ class ckl2poamGUI:
         ws1.title = 'POA&M'
         ws2 = wb.create_sheet()
         ws2.title = 'Remediation'
-        c1Values = ['Weakness','CAT','Severity','IA Control and Impact Code','POC','Resources Required','Scheduled Completion Date','Milestones with Completion Dates','Milestone Changes','Source Identifying Weakness','Status','Comments']
-        c2Values = ['Weakness','CAT','Severity','IA Control and Impact Code','Source Identifying Weakness','Vulnerability Discussion','Check Content','Fix Text','Comments']
+        c1Values = ['Weakness','CAT','Severity','IA Control and Impact Code','POC','Resources Required','Scheduled Completion Date','Milestones with Completion Dates','Milestone Changes','Source Identifying Weakness','Status','Finding Details','Comments']
+        c2Values = ['Weakness','CAT','Severity','IA Control and Impact Code','Source Identifying Weakness','Vulnerability Discussion','Check Content','Fix Text','Finding Details','Comments']
         for i in range(len(c1Values)):
             ws1.cell(row = 0, column = i).value = c1Values[i]
         for i in range(len(c2Values)):
@@ -78,6 +78,7 @@ class ckl2poamGUI:
         for vuln in vulnTag:
             status = vuln.getElementsByTagName('STATUS')[0].firstChild.data
             if status == 'Not_Reviewed' or status == 'Open':
+    
                 severity = vuln.getElementsByTagName('ATTRIBUTE_DATA')[1].firstChild.data
                 if severity == 'high':
                     cat = 'CAT I'
@@ -91,44 +92,56 @@ class ckl2poamGUI:
                 if vuln.getElementsByTagName('ATTRIBUTE_DATA')[2].hasChildNodes():
                     groupTitle = vuln.getElementsByTagName('ATTRIBUTE_DATA')[2].firstChild.data
                 else:
-                    comments = ''
+                    groupTitle = ''
 
                 if vuln.getElementsByTagName('ATTRIBUTE_DATA')[3].hasChildNodes():
                     ruleid = vuln.getElementsByTagName('ATTRIBUTE_DATA')[3].firstChild.data
                 else:
                     ruleid = ''
+
                 if vuln.getElementsByTagName('ATTRIBUTE_DATA')[5].hasChildNodes():
                     ruleTitle = vuln.getElementsByTagName('ATTRIBUTE_DATA')[5].firstChild.data
                 else:
                     ruleTitle = ''
+
                 if vuln.getElementsByTagName('ATTRIBUTE_DATA')[6].hasChildNodes():
                     vulnDiscuss = vuln.getElementsByTagName('ATTRIBUTE_DATA')[6].firstChild.data
                 else:
                     vulnDiscuss = ''
+
                 if vuln.getElementsByTagName('ATTRIBUTE_DATA')[7].hasChildNodes():
                     iaControls = vuln.getElementsByTagName('ATTRIBUTE_DATA')[7].firstChild.data
                 else:
                     iaControls = ''
+
                 if vuln.getElementsByTagName('ATTRIBUTE_DATA')[8].hasChildNodes():
                     chkContent = vuln.getElementsByTagName('ATTRIBUTE_DATA')[8].firstChild.data
                 else:
                     chkContent = ''
+
                 if vuln.getElementsByTagName('ATTRIBUTE_DATA')[9].hasChildNodes():
                     fixText = vuln.getElementsByTagName('ATTRIBUTE_DATA')[9].firstChild.data
                 else:
                     fixText = ''
+
+                if vuln.getElementsByTagName('FINDING_DETAILS')[0].hasChildNodes():
+                    findDetails = vuln.getElementsByTagName('FINDING_DETAILS')[0].firstChild.data
+                else:
+                    findDetails = ''
+
                 if vuln.getElementsByTagName('COMMENTS')[0].hasChildNodes():
                     comments = vuln.getElementsByTagName('COMMENTS')[0].firstChild.data
                 else:
                     comments = ''
                     
-                poamValues = [ruleTitle,cat,severity,iaControls,'','','','','',groupTitle + '\n' + ruleid,status,' ']
-                remeValues = [ruleTitle,cat,severity,iaControls,groupTitle + '\n' + ruleid,vulnDiscuss,chkContent,fixText,comments]
+                poamValues = [ruleTitle,cat,severity,iaControls,'','','','','',groupTitle + '\n' + ruleid,status,findDetails,comments]
+                remeValues = [ruleTitle,cat,severity,iaControls,groupTitle + '\n' + ruleid,vulnDiscuss,chkContent,fixText,findDetails,comments]
                 for i in range(len(poamValues)):
                     ws1.cell(row = rowCounter, column = i).value = poamValues[i]
                 for i in range(len(remeValues)):
                     ws2.cell(row = rowCounter, column = i).value = remeValues[i]
                 rowCounter = rowCounter + 1
+
         wb.save(self.fout)
         sys.exit(0)
     
